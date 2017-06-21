c1c8b8d7fc99be053ba4327066458e576d3a3f5e
Magno Junior <magnojr@magno.local>
2017-06-21 11:08:06 -0300

    lint

--- client/src/js/components/RatePanel/UserAvaliationControlPanel.spec.js
+++ client/src/js/components/RatePanel/UserAvaliationControlPanel.spec.js
@@ -2,25 +2,20 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import {expect, should} from 'chai';
 import sinon from 'sinon';
-
-
 import UserAvaliationControlPanel from "./UserAvaliationControlPanel"
 
+describe ('<UserAvaliationControlPanel />', function () { 
 
-
-describe('<UserAvaliationControlPanel />', function () { 
-
-	it('should render avaliation panel if it is not avaliated', function () {
+	it ('should render avaliation panel if it is not avaliated', function () {
 	
 		const wrapper = shallow(<UserAvaliationControlPanel />);
+        wrapper.setState({"avaliated": false});		
 
-        wrapper.setState({"avaliated": false});
-		
 		expect(wrapper.find('#panelAvaliation')).to.have.length(1);
 		
 	});
 
-    it('should render error avaliation panel if some error occurs after avaliation', function () {
+    it ('should render error avaliation panel if some error occurs after avaliation', function () {
 
         const wrapper = shallow(<UserAvaliationControlPanel />)
         wrapper.setState({"avaliated": false});
@@ -31,7 +26,7 @@ describe('<UserAvaliationControlPanel />', function () {
 
     });
 
-    it('should show confirmation message after avaliation if everything is ok', function(){
+    it ('should show confirmation message after avaliation if everything is ok', function() {
 
         const wrapper = shallow(<UserAvaliationControlPanel />)
         wrapper.setState({"avaliated": true});
@@ -42,10 +37,9 @@ describe('<UserAvaliationControlPanel />', function () {
 
     })
 
-    it('shouldcreate a new avaliation when clicked Confirm', function () {
+    it ('shouldcreate a new avaliation when clicked Confirm', function () {
 
-        var create = sinon.stub(UserAvaliationControlPanel.prototype, 'create');
-
+        const create = sinon.stub(UserAvaliationControlPanel.prototype, 'create');
         const wrapper = shallow(<UserAvaliationControlPanel />)
         wrapper.setState({"avaliated": false});
         wrapper.setState({stars: "5"});
@@ -53,9 +47,9 @@ describe('<UserAvaliationControlPanel />', function () {
         wrapper.setState({user_name: "test user"})
         wrapper.setState({user_email: "invalid@mail.com"})
         wrapper.find('button').simulate('click');
+        
 		expect(create.calledOnce).to.equal(true);
 
     });
-
  
 });
