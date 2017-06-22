4d1f2e6ff4797066af7c04e91de78f90e4e3f170
Magno Junior <magnojr@magno.local>
2017-06-22 00:22:23 -0300

    update the docker-compose

--- client/src/js/components/RatePanel/UserAvaliationControlPanel.js
+++ client/src/js/components/RatePanel/UserAvaliationControlPanel.js
@@ -100,7 +100,7 @@ export default class UserAvaliationControlPanel extends React.Component {
                                 <input class="star star-4" id="star-4" type="radio" ref="stars" name="stars" defaultValue="4" checked={this.state.stars == 4}  onChange={this.handleStarChange} /> <label class="star star-4" for="star-4"></label>
                                 <input class="star star-3" id="star-3" type="radio" ref="stars" name="stars" defaultValue="3" checked={this.state.stars == 3}  onChange={this.handleStarChange} /> <label class="star star-3" for="star-3"></label>
                                 <input class="star star-2" id="star-2" type="radio" ref="stars" name="stars" defaultValue="2" checked={this.state.stars == 2}  onChange={this.handleStarChange} /> <label class="star star-2" for="star-2"></label>
-                                <input class="star star-1" id="star-1" type="radio" ref="stars" name="stars" defaultValue="1" checked={this.state.stars == 1}  onChange={this.handleStarChange} /> <label class="star star-1" for="star-1"></label>								
+                                <input class="star star-1" id="star-1" type="radio" ref="stars" name="stars" defaultValue="1" checked={this.state.stars == 1}  onChange={this.handleStarChange} /> <label class="star star-1" for="star-1"></label>	
                         </div>
                         <div class="form-group">
                             <label>Comment</label>
